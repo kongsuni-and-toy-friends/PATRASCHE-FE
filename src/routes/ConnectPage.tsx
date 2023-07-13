4027592527a2b0b8145afccee1c5cbddef8c4eb2
kkanghh <attsky2@gmail.com>
2023-07-13 18:33:57 +0900

    :sparkles:Feat: 네트워크 연결 페이지 view 구현
    네트워크 연결 페이지 view 구현

---
+++ src/routes/ConnectPage.tsx
@@ -0,0 +1,22 @@
+const ConnectPage: React.FC = () => {
+  return (
+    <div className="fixed top-0 left-0 w-[100vw] h-[100vh] bg-white [&_input]:border-b [&_input]:border-black [&_input]:outline-none">
+      <h1>네트워크 연결</h1>
+      <div>
+        <label className="block" htmlFor="wifi">
+          Wi-Fi SSID
+        </label>
+        <input type="text" id="wifi" name="wifi" />
+      </div>
+      <div>
+        <label className="block" htmlFor="password">
+          비밀번호
+        </label>
+        <input type="text" id="password" name="password" />
+      </div>
+      <button>설정</button>
+    </div>
+  );
+};
+
+export default ConnectPage;
